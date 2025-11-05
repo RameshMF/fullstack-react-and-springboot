2a8aa6b0be33e3e9e0de0370514c0db59c2b6b79
Ramesh Fadatare <ramesh24fadatare@gmail.com>
2025-11-05 10:55:27 +0530

    Upgraded to Spring Boot 4 and Spring Security 7

--- todo-management/todo-jwt/todo-management/src/main/java/net/javaguides/todo/security/JwtTokenProvider.java
+++ todo-management/todo-jwt/todo-management/src/main/java/net/javaguides/todo/security/JwtTokenProvider.java
@@ -1,14 +1,13 @@
 package net.javaguides.todo.security;
 
-import io.jsonwebtoken.*;
+import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
-import net.javaguides.todo.exception.TodoAPIException;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpStatus;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
 
+import javax.crypto.SecretKey;
 import java.security.Key;
 import java.util.Date;
 
@@ -19,52 +18,46 @@ public class JwtTokenProvider {
     private String jwtSecret;
 
     @Value("${app.jwt-expiration-milliseconds}")
-    private long jwtExpirationDate;
+    private long jwtExpirationMilliseconds;
 
-    // Generate JWT token
+    // Create generate token utility method
     public String generateToken(Authentication authentication){
+
         String username = authentication.getName();
-
         Date currentDate = new Date();
+        Date expireDate = new Date(currentDate.getTime() + jwtExpirationMilliseconds);
 
-        Date expireDate = new Date(currentDate.getTime() + jwtExpirationDate);
-
-        String token = Jwts.builder()
-                .setSubject(username)
-                .setIssuedAt(new Date())
-                .setExpiration(expireDate)
+        return Jwts.builder()
+                .subject(username)
+                .issuedAt(new Date())
+                .expiration(expireDate)
                 .signWith(key())
                 .compact();
 
-        return token;
     }
 
     private Key key(){
-        return Keys.hmacShaKeyFor(
-                Decoders.BASE64.decode(jwtSecret)
-        );
+        return Keys.hmacShaKeyFor(Decoders.BASE64.decode(jwtSecret));
     }
 
-    // Get username from JWT token
+    // Get username from token
     public String getUsername(String token){
-        Claims claims = Jwts.parserBuilder()
-                .setSigningKey(key())
+
+        return Jwts.parser()
+                .verifyWith((SecretKey) key())
                 .build()
-                .parseClaimsJws(token)
-                .getBody();
-
-        String username = claims.getSubject();
-
-        return username;
+                .parseSignedClaims(token)
+                .getPayload()
+                .getSubject();
     }
 
-    // Validate JWT Token
+
+    // validate token
     public boolean validateToken(String token){
-            Jwts.parserBuilder()
-                    .setSigningKey(key())
-                    .build()
-                    .parse(token);
-            return true;
+        Jwts.parser()
+                .verifyWith((SecretKey) key())
+                .build()
+                .parse(token);
+        return true;
     }
-
 }
